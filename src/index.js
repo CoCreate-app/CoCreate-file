0143d76bdd4ef13106f390c3568f528a2ed1dbfc
frankpagan <frank@cocreate.app>
2026-02-09 11:13:20 -0600

    fix: root factory variable Module

--- src/index.js
+++ src/index.js
@@ -24,15 +24,15 @@
 
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
-        define(["./client"], function (CoCreateFile) {
-            return factory(CoCreateFile)
+        define(["./client"], function (Module) {
+            return factory(Module)
         });
     } else if (typeof module === 'object' && module.exports) {
-        const CoCreateFile = require("./server.js")
-        module.exports = factory(CoCreateFile);
+        const Module = require("./server.js")
+        module.exports = factory(Module);
     } else {
         root.returnExports = factory(root["./client.js"]);
     }
-}(typeof self !== 'undefined' ? self : this, function (CoCreateFile) {
-    return CoCreateFile;
+}(typeof self !== 'undefined' ? self : this, function (Module) {
+    return Module;
 }));
